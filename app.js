175fd5b385d2d1198314b07957bee55ff1e68c9f
luislabradaa <swirly.olive@gmail.com>
2025-03-20 13:34:33 -0600

    v 0.01

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
+var debug = require('debug')('fluxdev:server');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,5 +39,50 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Setup server configuration
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Create HTTP server
+var server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+// Normalize a port into a number, string, or false
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) return val; // named pipe
+  if (port >= 0) return port; // port number
+  return false;
+}
+
+// Event listener for HTTP server "error" event
+function onError(error) {
+  if (error.syscall !== 'listen') throw error;
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+// Event listener for HTTP server "listening" event
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 module.exports = app;
