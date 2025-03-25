1e8c5ddf8c4dc08b0561aea8b550652e2a2e7b07
luislabradaa <swirly.olive@gmail.com>
2025-03-25 00:22:28 -0600

    v 0.02

--- netlify/functions/api.js
+++
@@ -1,5 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../../app'); // Importa tu app desde app.js
-
-// Exporta tu aplicación como una función serverless
-module.exports.handler = serverless(app);
